82d943f1a576259b689493994f96d1d7357e16a7
Jacob Counts <infamous213@gmail.com>
2021-01-14 18:00:20 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://randomuser.me/api/?results=200&nat=us";
+
+class App extends React.Component {
+  state = {
+    employees: [],
+  };
+
+  componentDidMount() {
+    fetch(url)
+    .then(data => data.json())
+    .then(data => this.setState({ employees: data.results }));
+  }
+
+  render() {
+    const { employees } = this.state;
+    return (
+      <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First</th>
+            <th>Last</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.login.uuid}>
+              <td>
+                <img src={employee.picture.thumbnail} alt="img"></img>
+              </td>
+              <td>{employee.name.first}</td>
+              <td>{employee.name.last}</td>
+              <td>{employee.email}</td>
+              <td>{employee.cell}</td>
+              <td>{employee.dob.age}</td>
+              <td>{employee.location.city}, {employee.location.state}</td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+        </>
+    );
+  }
 }
+
 
 export default App;
